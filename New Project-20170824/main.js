81ea837abdddf51f541bba3d394d53556bfc50df
Varun <sriram.varun672@gmail.com>
2017-08-24 15:50:06 -0500

    Update main.js

--- New Project-20170824/main.js
+++ New Project-20170824/main.js
@@ -1,2 +1,26 @@
 /* Simple Hello World in Node.js */
 console.log("Hello World");
+var request = require('request');
+
+var headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'text/plain',
+    'Authorization': 'Basic YzZjZTgwMTAtMmQ1MS00MjQwLThlYTEtY2E3ZmVmOTgxMjRhOlZ0czZ4bVdKN0ZVdw=='
+};
+
+var options = {
+    url: 'https://watson-api-explorer.mybluemix.net/retrieve-and-rank/api/v1/solr_clusters/sc095bfcdb_c4b4_4fc6_b182_7493ceac558e/solr/soho/fcselect?ranker_id=7ff701x33-rank-1608&q=how%20do%20i%20set%20up%20quantum%20view&wt=json',
+    method: 'POST',
+    headers: headers
+};
+
+var rnr_result = '';
+
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body);
+        rnr_result = body;
+    }
+}
+
+request(options, callback);
